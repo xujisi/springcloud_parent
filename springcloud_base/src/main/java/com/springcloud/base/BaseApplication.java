a366ab0352539dc1be000c3bbc794faaa3280e47
Spy_ai_xjs <562683719@qq.com>
2020-04-27 23:41:54 +0800

    第二次提交(加入了eurake) ——xjs

--- springcloud_base/src/main/java/com/springcloud/base/BaseApplication.java
+++ springcloud_base/src/main/java/com/springcloud/base/BaseApplication.java
@@ -3,7 +3,6 @@ package com.springcloud.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
